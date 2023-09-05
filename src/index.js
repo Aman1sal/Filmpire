456e11f7603017a3883f2b7633ff190b9147dc3f
Aman <aman8958bansal@gmail.com>
2023-09-05 20:00:20 +0530

    initial files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme ={theme}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
